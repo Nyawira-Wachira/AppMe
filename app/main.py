6da762ff3d31ba3473d3f0f732357f2e8ab3e583
ABBY <abbyW@moringa.com>
2022-05-09 16:42:43 +0300

    Added register form for new user sign in.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash,redirect, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -33,9 +33,12 @@ def home():
 def movies():
     return render_template('movie.html',title='Movies') 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm() 
+    if form.validate_on_submit():
+        flash(f' Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/Login')

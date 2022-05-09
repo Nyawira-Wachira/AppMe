9699e343275aead6d76813d037e875f501472583
ABBY <abbyW@moringa.com>
2022-05-09 08:45:51 +0300

    App initialization.

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Linda Jones',
+        'title': 'First Pitch',
+        'description' : 'First Pitch description',
+        'date_posted': 'May 6, 2022'
+    },
+    {
+        'author': 'Peter Woo',
+        'title': 'SecondPitch',
+        'description' : '   Second Pitch description',
+        'date_posted': 'May 7, 2022'
+    },
+    {
+        'author': '',
+        'title': 'Third Pitch',
+        'description' : 'Third Pitch description',
+        'date_posted': 'May 8, 2022'
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html',posts=posts) 
+
+if __name__ == "__main__":
+        app.run(debug=True)    

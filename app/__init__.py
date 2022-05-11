3d30b426c18971342957cd88c3f5aa136428d069
ABBY <abbyW@moringa.com>
2022-05-12 00:51:20 +0300

    App changes for heroku deployment.

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '0b6bd163c27f7d7512077a91'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://irhbzsslwwjlid:84700a835c2958615dfca352a0a330b8c168b3ddacec52bb78d5b4a5327d8bf2@ec2-54-158-247-210.compute-1.amazonaws.com:5432/da2e41gniio6s9'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['MAIL_PORT'] = 465
